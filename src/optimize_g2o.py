d1358e365dfee91eefc414a394bd333b3cff5a0b
andrewnijmeh <andrewnijmeh1@gmail.com>
2021-05-25 14:23:52 -0400

    start g2opy optimization, create sparse instance

--- src/optimize_g2o.py
+++ src/optimize_g2o.py
@@ -1,3 +1,12 @@
-import g2o
+import g2opy
+import numpy as nq
 import numpy as nq
 
+
+def optimize(frames, points, local_window, fix_points, verbose=False, rounds=100):
+  if local_window is None:
+    local_frames = frames 
+  else:
+    local_frames = frames[-local_window:]
+
+  opt = g2opy.SparseOptimizer() 
